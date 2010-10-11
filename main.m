4077a212e9bd4a94aea037ba3e931b600b4234fe
kt <pth1409@gmail.com>
2010-10-11 15:48:24 +0900

    z

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  KtSonic
+//
+//  Created by 최지원  on 10. 10. 6..
+//  Copyright 2010 KT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
